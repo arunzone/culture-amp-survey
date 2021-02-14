360871208bb7b7bda87c38dbd9bd5d5bd0514d85
arun <aruncontacts@gmail.com>
2021-02-15 10:08:08 +1100

    Exceptions handled and displayed message ain console

--- src/main/java/com/cultureamp/App.java
+++ src/main/java/com/cultureamp/App.java
@@ -1,18 +1,23 @@
 package com.cultureamp;
 
+import com.cultureamp.report.ParticipationReport;
 import com.cultureamp.report.ReportFactory;
+import com.cultureamp.report.SurveyRatingReport;
 
 public class App {
   public static void main(String[] args) {
     ReportFactory reportFactory = new ReportFactory(args);
-    
-    reportFactory.
-        getParticipationReport().
-        generate();
+    try {
 
-    reportFactory.
-        getRatingReport().
-        generate();
+      ParticipationReport participationReport = reportFactory.getParticipationReport();
+      participationReport.generate();
+
+      SurveyRatingReport ratingReport = reportFactory.getRatingReport();
+      ratingReport.generate();
+
+    } catch (Exception exception) {
+      System.out.println(exception.getMessage());
+    }
   }
 
 }
--- src/test/java/com/cultureamp/AppTest.java
+++ src/test/java/com/cultureamp/AppTest.java
@@ -27,4 +27,14 @@ public class AppTest {
 
     assertThat(text, is("Participation report\nParticipation percentage: 83.33\nTotal participant counts: 5\n\nRating report\nI like the kind of work I do.: 4.6\nIn general, I have the resources (e.g., business tools, information, facilities, IT or functional support) I need to be effective.: 5\nWe are working at the right pace to meet our goals.: 5\nI feel empowered to get the work done for which I am responsible.: 3.6\nI am appropriately involved in decisions that affect my work.: 3.6\n"));
   }
+
+  @Test
+  public void shouldHandleException() throws Exception {
+
+    String text = tapSystemOut(() -> {
+      App.main(new String[]{"invalid/FileName"});
+    });
+
+    assertThat(text, is("Invalid input file: invalid/FileName-responses\n"));
+  }
 }
